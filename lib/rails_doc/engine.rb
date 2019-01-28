842ddebcbf5a943c94511d47c7433ff3c16a6fc9
qinmingyuan <mingyuan0715@foxmail.com>
2019-01-28 14:04:50 +0800

    new dummy url

--- lib/rails_doc/engine.rb
+++ lib/rails_doc/engine.rb
@@ -1,4 +1,20 @@
+require 'rails_com'
+
 module RailsDoc
   class Engine < ::Rails::Engine
+
+    config.generators do |g|
+      g.rails = {
+        assets: false,
+        stylesheets: false,
+        helper: false
+      }
+      g.test_unit = {
+        fixture: true,
+        fixture_replacement: :factory_bot
+      }
+      g.templates.unshift File.expand_path('lib/templates', RailsCom::Engine.root)
+    end
+
   end
 end
